d238142f1b6ddb07081bbc792413ea84fe8cde20
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-09-16 16:36:18 +0200

    finished Dark Mode

--- main.js
+++ main.js
@@ -83,6 +83,57 @@ equal.onclick = (e) => {
     nb = false;
   }
 };
-toggle.onclick = () => {
-  span.classList.toggle("active");
+
+// DarK Mode
+window.onload = function () {
+  darkMode(localStorage.getItem("active"));
 };
+toggle.addEventListener("click", () => {
+  span.classList.toggle("active");
+  if (span.classList.contains("active")) {
+    localStorage.setItem("active", "dark");
+    darkMode(localStorage.getItem("active"));
+  } else {
+    localStorage["active"] = "white";
+    darkMode(localStorage.getItem("active"));
+  }
+});
+
+function darkMode(value) {
+  if (value == "dark") {
+    span.classList.add("active");
+    document.documentElement.style.setProperty("--bg-body-color", "#17062a");
+    document.documentElement.style.setProperty("--result-color", "#1e0836");
+    document.documentElement.style.setProperty("--bg-row-btn-color", "#1e0836");
+    document.documentElement.style.setProperty("--bg-btn-color", "#321b4c");
+    document.documentElement.style.setProperty("--border-btn-color", "#7b2291");
+    document.documentElement.style.setProperty("--del-reset-color", "#56077d");
+    document.documentElement.style.setProperty(
+      "--border-del-reset-color",
+      "#a923db"
+    );
+    document.documentElement.style.setProperty("--bg-equal-color", "#00dfcf");
+    document.documentElement.style.setProperty(
+      "--border-equal-color",
+      "#89e8f2"
+    );
+    document.documentElement.style.setProperty("--black-color", "#f3de60");
+  } else {
+    document.documentElement.style.setProperty("--bg-body-color", "#e6e6e6");
+    document.documentElement.style.setProperty("--result-color", "#eeeeee");
+    document.documentElement.style.setProperty("--bg-row-btn-color", "#d3cdcd");
+    document.documentElement.style.setProperty("--bg-btn-color", "#e5e4e0");
+    document.documentElement.style.setProperty("--border-btn-color", "#a29e96");
+    document.documentElement.style.setProperty("--del-reset-color", "#378285");
+    document.documentElement.style.setProperty(
+      "--border-del-reset-color",
+      "#24666d"
+    );
+    document.documentElement.style.setProperty("--bg-equal-color", "#ce4030");
+    document.documentElement.style.setProperty(
+      "--border-equal-color",
+      "#902720"
+    );
+    document.documentElement.style.setProperty("--black-color", "#383732");
+  }
+}
